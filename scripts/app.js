fc756195da9cb56acb97dd2d7f1f2ffcbcce2619
Ali <me74242@gmail.com>
2022-07-17 18:17:32 +0430

    test

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,22 @@
+const hambeugerMenuBtn = document.getElementById('hambeugerMenu')
+const hambeugerMenuContainer = document.getElementById("hamburgerMenuContainer")
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth < 600) {
+        hambeugerMenuContainer.style.display = "none";
+        document.getElementById("header-items").style.display = "none";
+        return
+    }
+    else {
+        hambeugerMenuBtn.style.display = 'none'
+    }
+})
+
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     width: '300',
     loop: true,
     speed: 400,
     spaceBetween: 20,
-    // Navigation arrows
     navigation: {
         nextEl: '#next',
         prevEl: '#prev',
