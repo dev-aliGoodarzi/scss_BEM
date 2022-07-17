8f21c6801247089adde547bc691232d3ce66705a
Ali <me74242@gmail.com>
2022-07-17 15:24:41 +0430

    release

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-const starElements = document.querySelectorAll('.star.rating');
-starElements.forEach(star => {
-    star.addEventListener('click', function (e) {
-        let starEl = e.currentTarget;
-        console.log(starEl.parentNode)
-        starEl.parentNode.setAttribute('data-stars', starEl.dataset.rating);
-    });
-})
