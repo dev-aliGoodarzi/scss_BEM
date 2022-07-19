9c70ba1db0d954eaa3433e3ed75b6d856b5b206f
Ali <me74242@gmail.com>
2022-07-19 05:27:29 +0430

    addAnimationOnNavbarInMobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,20 @@
-const hambeugerMenuBtn = document.getElementById('hambeugerMenu')
+const hambeugerMenuBtn = document.getElementById('hambeugerMenuBtn')
 const hambeugerMenuContainer = document.getElementById("hamburgerMenuContainer")
+const hamburgerMenuLinksMasterContainer = document.getElementById("ham_linksMaster")
 document.addEventListener("DOMContentLoaded", () => {
     if (window.innerWidth < 400) {
-        hambeugerMenuContainer.style.display = "none";
         document.getElementById("header-items").style.display = "none";
         return
     }
     else {
+        hambeugerMenuContainer.style.display = "none";
         hambeugerMenuBtn.style.display = 'none'
     }
 })
 
 const swiper = new Swiper('.swiper', {
-    direction: (window.innerWidth < 400 ? 'horizontal' : "vertical"),
-    width: '300',
+    direction: 'horizontal',
+    width: window.innerWidth < 400 ? window.innerWidth - 10 : '300',
     loop: true,
     speed: 400,
     spaceBetween: 20,
@@ -22,3 +23,24 @@ const swiper = new Swiper('.swiper', {
         prevEl: '#prev',
     },
 });
+
+
+hambeugerMenuBtn.addEventListener("click", () => {
+    hambeugerMenuContainer.style.opacity = 1;
+    hambeugerMenuContainer.style.visibility = "visible"
+    hambeugerMenuContainer.style.zIndex = 50;
+    setTimeout(() => {
+        hamburgerMenuLinksMasterContainer.style.opacity = 1;
+        hamburgerMenuLinksMasterContainer.style.visibility = "visible";
+        hamburgerMenuLinksMasterContainer.style.left = 0;
+    }, 200)
+})
+
+hambeugerMenuContainer.addEventListener("click", () => {
+    hamburgerMenuLinksMasterContainer.style.left = "-80%";
+    setTimeout(() => {
+        hambeugerMenuContainer.style.opacity = 0;
+        hambeugerMenuContainer.style.visibility = "hidden";
+        hambeugerMenuContainer.style.zIndex = 0;
+    }, 500)
+})
